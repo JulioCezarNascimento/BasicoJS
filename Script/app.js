6a17d65d65dd1a6e61e6ef7d5614d47506c6df6b
Júlio Cezar Nascimento <110551243+JulioCezarNascimento@users.noreply.github.com>
2023-08-09 15:54:37 -0300

    Primeiro teste usando variavies

--- Script/app.js
+++ Script/app.js
@@ -1,3 +1,11 @@
-import { apikey } from "./util";
+//import { apikey } from "./util";
+//import { apikey, abc as content } from "./util";
+//import * as util from "./util";
 
-console.log(apikey)
+
+//console.log(apikey);
+//console.log(content);
+let userMessage = "Hello world!";
+
+console.log(userMessage);
+console.log(userMessage);
