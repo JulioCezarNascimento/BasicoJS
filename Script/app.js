4ac5a173b4eadf509b3706c925c84bb8e5d9ea1d
JulioCezarNascimento <silvajuliocezar992@gmail.com>
2023-08-24 17:32:00 -0300

    Utilização do const para o user e export padrão.

--- Script/app.js
+++ Script/app.js
@@ -11,14 +11,24 @@
 //console.log(userMessage);
 //console.log(userMessage);
 
-function createGreeting(userName, message) {
+//function createGreeting(userName, message) {
     //console.log(userName);
     //console.log(message);
-    return "Hi, I'm " + userName + "." + message;
+    //return "Hi, I'm " + userName + "." + message;
+//}
+
+//const greeting1 = createGreeting("Michel");
+//console.log(greeting1);
+
+//const greeting2 = createGreeting("Maria","Hello, what are you?");
+//connsole.log(greeting2);
+
+const user = {
+    name: "Michel",
+    age: 30
+};
+
+export default () => { 
+    console.log('Hello!');
+    return userName + massage;
 }
-
-const greeting1 = createGreeting("Michel");
-console.log(greeting1);
-
-const greeting2 = createGreeting("Maria","Hello, what are you?");
-connsole.log(greeting2);
